8ea8623bd1ebc334075c79d84f6669f0be0514ad
jsp-hashicorp <jsp@hashicorp.com>
2021-12-03 21:00:13 +0900

    change body

--- example/main.js
+++ example/main.js
@@ -6,7 +6,7 @@ exports.handler = function (event, context, callback) {
     headers: {
       'Content-Type': 'text/html; charset=utf-8',
     },
-    body: '<b>시스코X하시코프와 함께! 하이브리드 클라우드 자동화 시작하기</b><hr><center><p>AWS Lambda 애플리케이션 배포</p><p>안녕하세요 현재 버전은 0_0_2 입니다.</p></center>',
+    body: '<h1>시스코X하시코프와 함께! 하이브리드 클라우드 자동화 시작하기</h1><hr><center><h2><b><p>AWS Lambda 애플리케이션 배포</p><p>안녕하세요 현재 버전은 0_0_2 입니다.</p></b></h2></center>',
   }
   callback(null, response)
 }
